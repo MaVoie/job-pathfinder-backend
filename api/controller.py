650ca16e6e721ae376417e010937c5d309fdeac4
Robert Maj <majrobert90@gmail.com>
2023-06-14 15:08:32 +0200

    working post message

--- api/controller.py
+++ api/controller.py
@@ -1,11 +1,18 @@
+from typing import List
 
 from fastapi import APIRouter, status
+from pydantic import BaseModel
 
+from model.recruitment_process import RecruitmentProcess
 from service.recruitment_process_reader import RecruitmentProcessReader
 
 router = APIRouter()
 
 
-@router.get("/processes/{process_id}", status_code=status.HTTP_202_ACCEPTED)
-async def select_position(process_id: str, selected_position: str):
+class SelectedPositionsBody(BaseModel):
+    selected_positions: List[str]
+
+
+@router.post("/processes/{process_id}", response_model=RecruitmentProcess, status_code=status.HTTP_202_ACCEPTED)
+async def select_position(process_id: str, selected_position: SelectedPositionsBody):
     return RecruitmentProcessReader.get_recruitment_process()
