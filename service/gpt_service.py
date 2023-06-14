b91ab6b613eb279f64e86d38bfeb51d203768346
Robert Maj <majrobert90@gmail.com>
2023-06-14 17:16:58 +0200

    adjust generate cover letter prompt

--- service/gpt_service.py
+++ service/gpt_service.py
@@ -5,7 +5,7 @@ from config import OPENAI_API_KEY
 from model.recruitment_process import RecruitmentProcess
 
 os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
-llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0)
+llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.9)
 
 
 class GptService:
@@ -17,10 +17,10 @@ class GptService:
         prompt = PromptTemplate(
             input_variables=["aboutMe", "position", "username"],
             template="Please generate two cover letters max. 200 words for me for given position: {position}. I have "
-                     "introduced"
+                     "introduced "
                      "myself as: {aboutMe}. "
-                     "My name is {username}. If my position does not fit to me, just send me phrase: Consider "
-                     "different position to apply for.",
+                     "My name is {username}."
+                     "If my position does not match my person, just do not generate cover letter.",
         )
         print(prompt.format(aboutMe=process.custom_msg, position=process.selected_positions[0], username=username))
         chain = LLMChain(llm=llm, prompt=prompt)
